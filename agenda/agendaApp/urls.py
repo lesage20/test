105d4e03a99d0e539aa32e4dbece59ca0688eeb7
angezanou00@gmail.com0 <angezanou00@gmail.com>
2021-02-19 11:51:28 +0100

    update

--- agenda/agendaApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('events', views.events, name='events'),
-    path('single_event', views.single_event, name='single_event')
-    
-]
--- agenda/news/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def news(request):
-    datas = {
-        
-    }
-    return render(request, 'events-news.html', datas)
